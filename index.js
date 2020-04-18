c39eeba4c15bab6c4f2aecd6982e410a0ccf2b61
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-04-18 20:44:49 -0300

    Adding Bootstrap

--- index.js
+++ index.js
@@ -5,32 +5,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/:nome/:lang', (req, res) => {
-    let nome = req.params.nome;
-    let lang = req.params.lang;
-    let msg = true;
-
-    let produtos = [
-        {nome: 'doritos', preco: 1.14},
-        {nome: 'coca', preco: 5},
-        {nome: 'alface', preco: 0.50}
-    ]
-    
-    res.render('index', {
-        nome,
-        lang,
-        empresa: 'ABC71',
-        msg,
-        produtos
-    });
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-app.get('/home', (req, res) => {
-    res.render('home');
-});
-
-app.get('/profile', (req, res) => {
-    res.render('principal/profile');
+app.get('/ask', (req, res) => {
+    res.render('ask');
 });
 
 app.listen(3333, () => { 
