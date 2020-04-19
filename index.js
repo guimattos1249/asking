ca78f70fab836cc6808554fd091e75efa40ce318
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-04-18 21:29:05 -0300

    sending form

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 //dizendo p/ o express usar o EJS como view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -11,6 +15,12 @@ app.get('/', (req, res) => {
 
 app.get('/ask', (req, res) => {
     res.render('ask');
+});
+
+app.post('/salvarpergunta', (req, res) => {
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(`Titulo:${title} Descricao: ${description}`);
 });
 
 app.listen(3333, () => { 
